7ae45df3ba6ee1fd2fe5c4f3551ce47682be8008
Alex Conder <alex.v.conder@gmail.com>
2022-05-06 22:35:06 -0300

    fix: correcting font-famlies import

--- styles/globals.ts
+++ styles/globals.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900;8..144,1000&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,200;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;1,400&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,200;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;1,400&display=swap');
   :root{
     --colorBlack: #090909;
     --colorDarkGreen: #203130;
@@ -10,10 +10,13 @@ export const GlobalStyle = createGlobalStyle`
     --colorGreen: #9DCCC5;  
   }  
   *{
-      font-family: 'Roboto Flex', sans-serif;
+      font-family: Roboto, sans-serif;
       box-sizing: border-box;
       margin: 0;
       padding: 0;
+  }
+  ul, ol{
+    list-style: none;
   }
   body {
     background: linear-gradient(#090909, #202130) no-repeat;
