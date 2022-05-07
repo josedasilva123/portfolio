e6fccf2849c8332e274802d9eabcd4bb8e03c375
Alex Conder <alex.v.conder@gmail.com>
2022-05-06 22:36:42 -0300

    fix: importing GlobalStorage in the right place

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { GlobalStorage } from '../../contexts/GlobalContext';
 import DesktopHeader from './Desktop';
 
 const Header: React.FC = () => {
   return (
      <>
-        <GlobalStorage>
+     
             <DesktopHeader />
-        </GlobalStorage>
+   
      </>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
+import { GlobalStorage } from "../contexts/GlobalContext";
 import { GlobalStyle } from "../styles/globals";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </>
+      <GlobalStorage>
+        <Header />
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </GlobalStorage>
   );
 }
 
