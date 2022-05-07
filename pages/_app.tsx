e7e21e5bf8cf7c0be55039f40ec424ce673ffc51
Alex Conder <alex.v.conder@gmail.com>
2022-05-07 11:46:03 -0300

    fix: fixing font families import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Header from "../components/Header";
 import { GlobalStorage } from "../contexts/GlobalContext";
 import { GlobalStyle } from "../styles/globals";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <GlobalStorage>
         <Header />
         <Component {...pageProps} />
         <GlobalStyle />
       </GlobalStorage>
+    </>
   );
 }
 
--- styles/globals.ts
+++ styles/globals.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,200;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;1,400&display=swap');
   :root{
     --colorBlack: #090909;
     --colorDarkGreen: #203130;
@@ -14,6 +13,9 @@ export const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
       margin: 0;
       padding: 0;
+  }
+  a{
+    text-decoration: none;
   }
   ul, ol{
     list-style: none;
