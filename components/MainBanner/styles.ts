fc9057a04148c42ef71e74024ba17b67a87a3576
Alex Conder <alex.v.conder@gmail.com>
2022-05-08 19:33:44 -0300

    feat: finishing MainBanner component

---
+++ components/MainBanner/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const MainBannerGrid = styled.section`
+    h2{
+        background: url('/Code.jpg');
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: rgba(255,255,255, .6);
+        background-size: cover;
+    }
+    img{
+        max-width: 100%;
+    }
+    button{
+        max-width: 400px;
+        @media (max-width: 768px){
+            width: 100%;
+        }
+    }
+`
