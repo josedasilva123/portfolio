ab362221ee9f8a0f8f3b9a0153ca79c676d7afa1
Alex Conder <alex@kenzie.com.br>
2022-05-14 16:45:09 -0300

    feat: card and tag components

--- styles/components/visual.tsx
+++ styles/components/visual.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { iPadding } from '../../interfaces/global';
 
 interface iPercentBar{
     className?: string; 
@@ -6,10 +7,37 @@ interface iPercentBar{
     backgroundColor?: 'white' | 'primary' | 'secondary';
 }
 
+interface iCard{
+  children?: React.ReactNode;
+  className?: string; 
+  padding?: iPadding;
+}
+
+interface iTag{
+  children?: React.ReactNode;
+  className?: string; 
+  tagType: 'solid' | 'outline',
+  tagColor: string;
+}
+
 export const BasePercentBar: React.FC<iPercentBar> = ({className}) => {
   return (
     <div className={className}>
         <span></span>    
     </div>
+  )
+}
+
+export const BaseCard: React.FC<iCard> = ({className, children}) => {
+  return (
+    <li className={className}>
+      {children}
+    </li>
+  )
+}
+
+export const BaseTag: React.FC<iTag> = ({className, children}) => {
+  return (
+    <span className={className}>{children}</span>
   )
 }
--- styles/visual.ts
+++ styles/visual.ts
@@ -1,23 +1,95 @@
-import styled from "styled-components";
-import { BasePercentBar } from "./components/visual";
+import styled, { css } from "styled-components";
+import { BaseCard, BasePercentBar, BaseTag } from "./components/visual";
 import { backgroundColor } from "./functions/visual";
 
+export const ThemeCard = styled(BaseCard)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  padding: ${(props) => props.padding?.all};
+
+  padding-top: ${(props) => props.padding?.top};
+  padding-bottom: ${(props) => props.padding?.bottom};
+  padding-left: ${(props) => props.padding?.left};
+  padding-right: ${(props) => props.padding?.right};
+
+  border-radius: 1rem;
+  border: 2px solid var(--colorPrimaryOpacity20);
+
+  animation: fadeIn 0.4s forwards;
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
 export const PercentBar = styled(BasePercentBar)`
-    width: 100%;
+  width: 100%;
+  height: 6px;
+
+  background: var(--colorDarkPrimary);
+
+  border-radius: 255px;
+  transition: 0.3s;
+  span {
+    display: block;
+
+    width: ${(props) => props.percentValue}%;
     height: 6px;
 
-    background: var(--colorDarkPrimary);
-
     border-radius: 255px;
-    transition: .3s;
-    span{
-        display: block;
-        
-        width: ${(props) => props.percentValue}%;        
-        height: 6px;
+    //Cor da barra
+    ${(props) => backgroundColor(props.backgroundColor)}
+  }
+`;
 
-        border-radius: 255px;
-        //Cor da barra
-        ${(props) => backgroundColor(props.backgroundColor)}
+export const ThemeTag = styled(BaseTag)`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: var(--PrimaryFont);
+  font-weight: 600;
+  font-size: 13px;
+
+  padding: 0 1rem;
+  height: 24px;
+
+  border-radius: 255px;
+
+  ${(props) => {
+    let color;
+
+    switch (props.tagColor) {
+      case "white":
+        color = "--colorWhite";
+        break;
+      case "primary":
+        color = "--colorPrimaryButton";
+        break;
+      case "secondary":
+        color = "--colorSecondaryButton";
+        break;
     }
-`
+
+    switch (props.tagType) {
+      case "solid":
+        return css`
+            background: var(${color});
+            color: var(--colorBlack);
+        `;
+      case "outline":
+        return css`
+            background: transparent;
+            color: var(${color});
+            border: 2px solid var(${color});
+        `; 
+    }
+  }}
+`;
