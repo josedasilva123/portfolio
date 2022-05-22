c2ed18dd8407aeb536b4659c71806f82817c0cd5
Alex Conder <alex@kenzie.com.br>
2022-05-22 16:11:26 -0300

    feat: creating testmonials block

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,68 @@
-import type { NextPage } from 'next';
-import AboutMe from '../components/AboutMe';
-import MainBanner from '../components/MainBanner';
-import Projects from '../components/Projects';
-import SoftSkills from '../components/SoftSkills';
-import Techs from '../components/Techs';
-import { gitHubProjects } from '../data/project';
+/* eslint-disable react-hooks/exhaustive-deps */
+import type { NextPage } from "next";
+import { useContext, useEffect, useState } from "react";
+import AboutMe from "../components/AboutMe";
+import MainBanner from "../components/MainBanner";
+import Projects from "../components/Projects";
+import SoftSkills from "../components/SoftSkills";
+import Techs from "../components/Techs";
+import Testmonials from "../components/Testmonials";
+import { TestmonialContext } from "../contexts/TestmonialContext";
+import { gitHubProjects } from "../data/project";
+import { ThemeModal } from "../styles/modal";
 
-interface iHome{
-  data?: any[],
-  error?: string; 
+interface iHome {
+  repositories?: any[];
+  testmonials: any[];
+  repoError?: string;
 }
 
-const Home: NextPage<iHome> = ({ data, error }) => {
+const Home: NextPage<iHome> = ({ repositories, repoError, testmonials }) => {
+  const {setTestmonials} = useContext(TestmonialContext);
+  const [modal, setModal] = useState(true);
+
+  useEffect(() => {
+    setTestmonials(testmonials);
+  }, [])
+
   return (
     <>
       <MainBanner />
       <AboutMe />
       <Techs />
       <SoftSkills />
-      <Projects repo={data} error={error} />
+      <Projects repo={repositories} error={repoError} />
+      <Testmonials />
+      <ThemeModal active={modal} setActive={setModal}>
+        teste
+      </ThemeModal>
     </>
-  )
-}
+  );
+};
 
 export async function getStaticProps() {
   try {
-      const res = await fetch(`https://api.github.com/users/josedasilva123/repos`)
-      const data = await res.json();
-      const filteredData = data.filter(( repo: any ) => gitHubProjects.find((ref) => ref === repo.name));        
-      return { props: { data: filteredData } };
+    //Fetch Github Repos
+    const repoRes = await fetch(
+      `https://api.github.com/users/josedasilva123/repos`
+    );
+    
+    const dataRes = await repoRes.json();
+    const filteredData = dataRes.filter((repo: any) =>
+      gitHubProjects.find((ref) => ref === repo.name)
+    );
+
+    //Fetch Testmonials
+    const resTest = await fetch(
+      `https://alex-conder-portfolio-api.herokuapp.com/sheets/rows/depoimentos`
+    )
+
+    const dataTest = await  resTest.json();
+
+    return { props: { repositories: filteredData, testmonials: dataTest.values } };
   } catch (error) {
-     return { props: { error: 'Não foi possível carregar o repositório'}} 
+    return { props: { repoError: "Não foi possível carregar o repositório" } };
   }
 }
 
-export default Home
+export default Home;
