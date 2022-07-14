0367cc6569989147df5c54cbf6b6723c8f6d5574
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-07-14 17:53:41 -0300

    fix: identation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,24 +12,21 @@ import Footer from "../components/Footer";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Providers>
-      <DefaultFonts />
-        <Colors />        
-        <GlobalStyle />
-        <Header />
-        <Component {...pageProps} />    
-        <Footer />    
-      </Providers>       
-    </>
-  );
+    return (
+      <>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,200;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;1,400&display=swap" rel="stylesheet"/>
+        </Head>
+        <Providers>
+        <DefaultFonts />
+          <Colors />        
+          <GlobalStyle />
+          <Header />
+          <Component {...pageProps} />    
+          <Footer />    
+        </Providers>       
+      </>
+    );
 }
 
 export default MyApp;
